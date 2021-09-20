66afbe5cde15e3ff9365fea5547b57daab5e6ed2
Phil Jirsa <phil.jirsa@microsoft.com>
2021-09-20 17:56:48 -0500

    Add api Azure Funtions

--- chestist-smart-app/src/components/Navigation/Navigation.tsx
+++ chestist-smart-app/src/components/Navigation/Navigation.tsx
@@ -1,8 +1,7 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import PatientSnapshot from 'components/PatientSnapshot';
-import DropDown from 'components/DropDown';
 
 import classes from './Navigation.module.scss';
 
@@ -11,18 +10,7 @@ type Option = {
   value: string;
 };
 
-const pathwayOptions = [
-  { label: 'Medications', value: 'meds' },
-  { label: 'Chart', value: 'chart' },
-];
-
 const Navigation: FC<{}> = () => {
-  const [pathway, setPathway] = useState<Option | ReadonlyArray<Option> | null>(null);
-
-  const onChangeHandler = (pathway: Option | ReadonlyArray<Option> | null): void => {
-    setPathway(pathway);
-  };
-
   return (
     <nav className={classes.navigation}>
       <div className={classes['navigation__left-panel']}>
@@ -32,13 +20,7 @@ const Navigation: FC<{}> = () => {
       </div>
 
       <div className={classes['navigation__right-panel']}>
-        <DropDown
-          label="Pathway:"
-          id="patient-view"
-          options={pathwayOptions}
-          selectedValue={pathway}
-          onChange={onChangeHandler}
-        />
+        
       </div>
     </nav>
   );
